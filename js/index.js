84556060ad4a08b6c0f5a467bdf9b2740c564639
daniwhkim <dani@daniwhkim.com>
2016-03-30 21:25:32 -0400

    edited js file

--- js/index.js
+++ js/index.js
@@ -69,31 +69,21 @@ $(document).ready(function() {
 
 	function reset() {
 		alert("Reset game!");
+// numberOfGuessesRemaining will be reset to 5
 		numberOfGuessesRemaining = 5;
 		$("#numberOfGuessesRemaining").text(numberOfGuessesRemaining);
 		$(".message").text("");
 		$("body").attr("class", "reset");
 		$(".message").attr("class", "message");
+// regenerate new secretNumber
 		secretNumber = getRandomIntInclusive();
 	};
 
 });
 
-// 	#numberOfGuessesRemaining will be reset to 0
-// 	regenerate new secretNumber
-// 	else if not the same, alert "Incorrect" and give clues in notification
-
-// 		depending on secretNumber, if userGuess is 1 to 20, notify "Higher" or "Lower"
-// 		depending on secretNumber, if userGuess is 21 to 40, notify "Higher" or "Lower"
-// 		depending on secretNumber, if userGuess is 41 to 60, notify "Higher" or "Lower"
-// 		depending on secretNumber, if userGuess is 61 to 80, notify "Higher" or "Lower"
-// 		depending on secretNumber, if userGuess is 81 to 100, notify "Higher" or "Lower"
 // make secretNumber the same value for 5 guesses
 // with each userGuess, the number of guess text reduced until 0, then the user is unable to play
 // 	if user tries to enter guess, notify "Game over!"
 // if user clicks on #reset button, clear #numberOfGuessesRemaining to 5
 // 	notification will clear
 // 	regenerate secretNumber
-
-// apply the "success" class to the element containing the notification message when the user correctly guesses the secret number
-// apply the "error" class to the element containing the notification message when the user runs out of guesses
