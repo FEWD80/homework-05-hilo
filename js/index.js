930a15d6f4ce4d8d7bd31319cc813cbc90314933
Ashley Levinson <ashleyglevinson@gmail.com>
2016-04-06 19:49:59 -0400

    HW Submission from Ashley Levinson

--- js/index.js
+++ js/index.js
@@ -1,103 +1,58 @@
-var computerGenerated; 
+var computerGenerated = getRandomInt(); 
 var userGenerated; 
-var counter = 0;
+var counter = 5;
 
-
-$(function(){
 
 // At the start of each game the app should randomly generate the secret number
 
-	$('#Guess').click('getRandomInt');
 
-			function getRandomInt(event) {
-				event.preventDefault();
-		 		return parseInt(Math.floor(Math.random() * 100));
-			}
-
-// Users will be given 5 guesses
-
-// If a user enters a guess and it is incorrect the following should happen:
-// A notification will be displayed informing the user that their guess was either too low or too high
-
-	
-function runComparison(userGenerated, computerGenerated){
-    if(userGenerated > 0 && userGenerated < 101){
-        console.log('valid number');
-        compareNumbers(userGenerated, userGenerated);
-    } else{
-        console.log('invalid number');
-    }
+function getRandomInt() {
+	return Math.floor(Math.random() * 100 + 1);
 }
 
-function compareNumbers(userGenerated, computerGenerated){
+
+$('#submit').click(compareNumbers);
+function compareNumbers(event){
+	event.preventDefault();
+
+	userGenerated = Number($('#guess').val());
+
+	changeCounter();
+
     if(userGenerated < computerGenerated){
         alert('Too Low!');
     } else if(userGenerated > computerGenerated){
         alert('Too High!');
-    } else{
+    } else {
         alert('You guessed the number!!');
     }
 }
 
 
 // The number of guesses remaining count will be decremented by 1
+function changeCounter(){
 
-	$("#numberOfGuessesRemaining").click('changeCounter')
+	if (counter > 0){
+		$("#numberOfGuessesRemaining").text(counter - 1);
+		counter = counter - 1;
+		$('#guess').val('');
 
-		function changeCounter(){
+		// counter -= 1;
+		// counter--;
+		console.log(counter);
+	}
 
-			if (counter < 6){
-				$("#numberOfGuessesRemaining").text("5" - counter);
-				console.log(counter);
-			}
+	else if (counter === 0){
+		alert("You're out of guesses");
+		reset();
+	}
+}
 
-			else if (counter = 6){
-				alert("You're out of guesses");
-			}
+function reset() {
+	counter = 5;
+	computerGenerated = getRandomInt();
+	$('#guess').val('');
+	$("#numberOfGuessesRemaining").text(counter);
+}
 
-		}
-
-	};
-
-
-
-// If a user enters a guess and it is correct the following should happen: (NOTE TO SELF: Include this in the "if" section)
-	// The number of guesses remaining count will be reset back to 5
-
-
-// If a user runs out of guesses (5) without correctly guessing the secret number the following should happen:
-	// A notification will be displayed informing they user that they ran out of guesses and the game was over
-
-// When users click on the reset button the following should happen:
-	// The number of guesses remaining should be reset to 5
-	// The message should be hidden
-	// Apply the "success" class to the element containing the notification message when the user correctly guesses the secret number
-	// Apply the "error" class to the element containing the notification message when the user runs out of guesses
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('#reset').click(reset);
