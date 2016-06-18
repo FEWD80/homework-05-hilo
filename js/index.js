d10d2c5ae5741f9b2bcea9866e6cbbae6a8a693d
chrislindor <chrislindor@gmail.com>
2016-06-18 13:56:32 -0400

    refactored game functions

--- js/index.js
+++ js/index.js
@@ -1,95 +1,109 @@
 /******** Global Varibles *******/
 var generatedNumber,
-    playerChoice,
-    choicesLeft = 5,
-    gameOver = false,
-    $guess = $('#guessButton'),
-    $guessLeft = $('#numberOfGuessesRemaining'),
-    $guessInput = $('#guess'),
+    $guess = $('#guess'),
     $message = $('.message'),
-    $reset = $('#reset'),
-    gameOverMessage = 'Game over press reset to play again',
-    guessToHi = 'Your guess was to high',
-    guessToLow = 'Your guess was to low',
-    correctGuessMessage = 'You win the number was ',
-    playerNumb;
-
+    turnsLeft = 5,
+    isGameOver = false;
 /******** Functions Begin *******/
+
+
+
+var message = {
+  notValid: function() {
+    $message.addClass('error');
+    $message.text('Enter a number between 1 and 100');
+    },
+  toHigh: function(number) {
+    $message.text( number + ' is to hi');
+  },
+  toLow: function(number) {
+    $message.text(number + ' is to low');
+  },
+  clear: function() {
+    $message.removeClass('success error');
+    $message.html('');
+  },
+  noTurnsLeft: function() {
+    $message.addClass('error');
+    $message.text('No more turns left the number was ' + generatedNumber);
+  },
+  playerWon: function() {
+    $message.addClass('success');
+    $message.html('Congragts the number was ' + generatedNumber);
+  },
+  gameOver: function() {
+    $message.removeClass('success error');
+    $message.text('Game Over please Reset');
+  }
+};
 
 // Genertaes number with an even distrubtion
 function randomNumber() {
   return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 }
 
+function validateNumb(numb) {
+  var vaildEntry;
+  numb = parseInt(numb);
+  if ( numb >= 1 && numb <= 100) {
+    validEntry = [true, numb];
+    return validEntry;
+  } else {
+    message.notValid();
+  }
+}
+
+var turns = {
+  track: function(validation) {
+    if (validation && turnsLeft > 0) {
+      turnsLeft--;
+    }
+    $('#numberOfGuessesRemaining').text(turnsLeft);
+  },
+  reset: function() {
+    turnsLeft = 5;
+  }
+};
+
+function runGame(validation, number) {
+  message.clear();
+  turns.track(validation);
+  if (isGameOver) {
+    message.gameOver();
+  } else if (validation && turnsLeft > 0) {
+    if (number === generatedNumber) {
+      message.playerWon();
+      isGameOver = true;
+      turnsLeft = 0;
+    } else if (number < generatedNumber) {
+      message.toLow(number);
+    } else if (number > generatedNumber) {
+      message.toHigh(number);
+    }
+  } else if (turnsLeft === 0) {
+    message.noTurnsLeft();
+    isGameOver = true;
+  }
+
+}
+
 generatedNumber = randomNumber();
 console.log('Computer number: ' + generatedNumber);
 
-// function to generate and validate player number
-// function decrease choicesLeft if number is valid so player wont lost turn on miss typed entry
-function playerGuess() {
-  playerChoice = parseInt($guessInput.val());
-  $guessInput.val(''); // clear input field
-  if (isNaN(playerChoice) || playerChoice < 1 || playerChoice > 100) {
-    // this if statement checks to see if the value is outside of paramaters
-    // and if it is a number.
-    $message.attr('class', 'error');
-    $message.text('Please enter a number between 1 and 100');
-  } else {
-    // if number is valid decrese decrease number of tries left
-    $message.attr('class', 'message');
-    $message.text('');
-    choicesLeft--;
-    return playerChoice;
-  }
-}
+/******** Game Begins *******/
 
-// function to validate game choice and print correct number
-function gameValidation() {
-  // Checks to see if game is already completed
-  if (gameOver) {
-    $message.attr('class', 'error');
-    $message.text(gameOverMessage);
-  } else {
-    // Checks to see if player number is higer lower or matches random number
-    if (playerNumb > generatedNumber) {
-      $message.text(guessToHi);
-    } else if (playerNumb < generatedNumber) {
-      $message.text(guessToLow);
-    } else if (playerNumb === generatedNumber) {
-      $message.attr('class', 'success');
-      $message.text(correctGuessMessage + generatedNumber);
-      gameOver = true;
-      choicesLeft = 0;
-      $guessLeft.text(choicesLeft);
-    }
-  }
-
-}
-
-
-// start game functions on click event
-$guess.click(function(event) {
+$('#guessForm').submit(function(event) {
   event.preventDefault();
-  if (choicesLeft === 0) {
-    gameOver = true;
-    gameValidation();
-  } else {
-    playerNumb = playerGuess();
-    $guessLeft.text(choicesLeft);
-    console.log('Player choice: ' + playerNumb);
-    gameValidation();
-  }
-
+  var playerChoice = validateNumb($guess.val());
+  console.log(playerChoice);
+  runGame(playerChoice[0], playerChoice[1]);
 });
-// Reset button fucntion
-$reset.click(function(event) {
-  event.preventDefault();
+
+$('#reset').click(function(event) {
+  isGameOver = false;
+  turnsLeft = 5;
+  $('#numberOfGuessesRemaining').text(turnsLeft);
+  message.clear();
   generatedNumber = randomNumber();
-  console.log('Computer number: ' + generatedNumber);
-  choicesLeft = 5;
-  $guessLeft.text(choicesLeft);
-  $guessInput.val('');
-  $message.text('');
-  $message.attr('class', 'message');
-  gameOver = false;
+  console.log(generatedNumber);
 });
